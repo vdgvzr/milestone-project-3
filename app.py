4b04aa3c4d18312764d263f43d61bd87e82a8b05
Shaun Lindsley <shaunlindsley@gmail.com>
2020-08-21 15:17:08 +0000

    Created home page for site and environment variable for mongo password

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from decouple import config
 
 app = Flask(__name__)
 
+DB_PASSWORD = config('PASSWORD')
+
+app.config["MONGO_DBNAME"] = 'pen_hub'
+app.config["MONGO_URI"] = 'mongodb+srv://vdgvzr:' + DB_PASSWORD + '@myfirstcluster.iop5x.mongodb.net/pen_hub?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/get_books')
+def get_tasks():
+    return render_template("books.html", books=mongo.db.books.find())
 
 
 if __name__ == '__main__':
