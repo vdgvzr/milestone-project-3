8b6386d5ab19f5f5934f1910311b53ebd200a9fd
Shaun Lindsley <shaunlindsley@gmail.com>
2020-08-21 19:18:33 +0000

    Added basic form elements

--- app.py
+++ app.py
@@ -16,8 +16,13 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_books')
-def get_tasks():
+def get_books():
     return render_template("books.html", books=mongo.db.books.find())
+
+
+@app.route('/add_book')
+def add_book():
+    return render_template("add-book.html")
 
 
 if __name__ == '__main__':
