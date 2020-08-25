e94a70bd1c381032b6d7131b11fe5ead34a2b95f
Shaun Lindsley <shaunlindsley@gmail.com>
2020-08-25 10:22:27 +0000

    Added random quote function to quote.js

--- static/js/quote.js
+++ static/js/quote.js
@@ -4,11 +4,17 @@ $(".quote-content").each(function() {
     quotes[count++]=$(this).text();
 });
 
+function randomQuote() {
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quote-box').innerHTML = quotes[randomNumber];
+}
+
+randomQuote();
+
 function change() {
-    if (count >= quotes.length) count = 0;
+    if (count >= quotes.length) count = randomQuote();
     $("#quote-box").html(quotes[count++]);
-    $("#quote-box")
-        .fadeIn("slow").animate({opacity: 0.9}, 10000).fadeOut("slow",
+    $("#quote-box").fadeIn("slow").animate({opacity: 0.8}, 10000).fadeOut("slow",
         function() {
             return change()
         });
