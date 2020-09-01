9d4b154a5bfa8fa9fce7533298b1dd54c3056fe5
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-01 20:06:53 +0000

    Added complete login functionality

--- app.py
+++ app.py
@@ -64,18 +64,16 @@ def login():
 
         if existing_user:
             if check_password_hash(
-                existing_user["password"],
-                request.form.get("password")
-            ):
+                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+                flash("Welcome, {}!".format(request.form.get("username")))
             else:
                 flash("Incorrect Username and/or Password")
-                return redirect(url_for("login"))
+                return(url_for("login"))
 
         else:
             flash("Incorrect Username and/or Password")
-            return redirect(url_for("login"))
+            redirect(url_for("login"))
     return render_template("login.html")
 
 
