c51f6ff4d956470b99d11527343533e9696d3c44
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-02 11:37:37 +0000

    Added visibility toggle to all password input fields

---
+++ static/js/password.js
@@ -0,0 +1,8 @@
+function passwordShow() {
+    let x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
