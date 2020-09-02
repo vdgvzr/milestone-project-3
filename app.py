d1364c3e948d5c2dce6d3970bbf17145e33c3c2f
Shaun Lindsley <shaunlindsley@gmail.com>
2020-09-02 10:25:11 +0000

    changed app config variables and updated .env and app requirements.txt

--- app.py
+++ app.py
@@ -9,15 +9,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-DB_PASSWORD = config('PASSWORD')
-SECRET_KEY = config('SECRET_KEY')
-
 app.config["MONGO_DBNAME"] = 'pen_hub'
-app.config["MONGO_URI"] = 'mongodb+srv://vdgvzr:' +\
-                           DB_PASSWORD +\
-                          '@myfirstcluster.iop5x.mongodb.net/'\
-                          'pen_hub?retryWrites=true&w=majority'
-app.secret_key = SECRET_KEY
+app.config["MONGO_URI"] = config('MONGO_URI')
+app.secret_key = config('SECRET_KEY')
 
 mongo = PyMongo(app)
 
